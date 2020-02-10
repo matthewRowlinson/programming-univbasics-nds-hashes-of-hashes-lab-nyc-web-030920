abc15e91227e7e28348b4c2a96afe27ec4b9f9fe
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-10 20:50:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,11 +49,11 @@ def naming_system
           label: "Family", sub_category:{
             label: "Genus", sub_category:{
               label:"Species", sub_category:{}
-            },
-          },
-        },
-      },
-    },
+            }
+          }
+        }
+      }
+    }
     
   }
 end
