e523bf787152bc14774dfa22da710c9fe037b588
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-10 20:46:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,7 +43,17 @@ def naming_system
   # HoH all the way to Species!
   {
     label: "Kingdom", 
-    sub_category: {label: "Phylum"},
+    sub_category: {label: "Phylum",
+      sub_category: {label: "Class", sub_category{
+        label: "Order", sub_category{
+          label: "Family", sub_category{
+            label: "Genus", sub_category{
+              label:"Species", sub_category{}
+            }
+          }
+        }
+      }
+    },
     
   }
 end
